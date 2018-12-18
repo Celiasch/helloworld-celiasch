920dec407edc87a7f2257b8e83c796aa1918b446
Celiasch <clea.eliasch@htl-salzburg.ac.at>
2018-12-18 10:02:24 +0100

    added test msg

--- Assets/MyGame/Scripts/HelloWorld.cs
+++ Assets/MyGame/Scripts/HelloWorld.cs
@@ -8,7 +8,7 @@ public class HelloWorld : MonoBehaviour {
 	void Start () {
 		Debug.Log ("Hello World");
 	}
-	
+	//Git Test
 	// Update is called once per frame
 	void Update () {
 		Debug.Log ("alive");
